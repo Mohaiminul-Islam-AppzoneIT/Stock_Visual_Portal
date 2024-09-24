0a341f6813366421714deccd6bf4826a038141c7
Mohaiminul-Islam-AppzoneIT <mohaiminul.islam@appzoneit.com>
2024-09-24 19:09:42 +0600

    All the code and files of the predicton and Model traing

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,14 +1,14 @@
-import './assets/css/style.css';
-import Main from './components/Main';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './components/Register';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import AuthProvider from './authProvider';
-import Dashboard from './components/Dashboard/Dashboard';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import "./assets/css/style.css";
+import Main from "./components/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import AuthProvider from "./authProvider";
+import Dashboard from "./components/Dashboard/Dashboard";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 function App() {
   return (
@@ -16,10 +16,31 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
-          <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path='/dashboard' element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
